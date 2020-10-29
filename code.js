ba526324939a5e64ab5979c22b965e78dcb929b9
wjhueston <wjhueston@gmail.com>
2020-10-28 22:08:36 -0500

    Lab 4 update!

--- code.js
+++ code.js
@@ -1,25 +1,116 @@
-var totaldue = 0
-function AddBook(){
-    var bookDaysLate = parseInt(prompt("Enter the number of days late", "1"))
-    totaldue += bookDaysLate * 0.25
-}
-function AddDVD(){
-    var dvdDaysLate = parseInt(prompt("Enter the number of days late", "1"))
-    totaldue += dvdDaysLate *0.50
-}
-function TotalOut(){
-    alert("You owe $" + totaldue.toFixed(2))
-}
-var totalCoworkers = 0
-function TotalCoworkers(){
-    totalCoworkers = parseInt(prompt("How many coworkers want pizza?", "1"))
-    var cheesePricePerCoworker = 15/totalCoworkers
-    var coworkerNumber = 1
-    while(totalCoworkers > 0){
-        var totalToppings = parseInt(prompt("How many toppings does Co-Worker " + coworkerNumber + " want?"))
-        var pizzaPricePerCoworker = (totalToppings * 1.25) + cheesePricePerCoworker
-        document.write("Co-Worker " + coworkerNumber + " owes: $" + pizzaPricePerCoworker.toFixed(2) + "<br>")
-        coworkerNumber++;
-        totalCoworkers--;
-    }
-}
+$(document).ready(
+    function(){
+        //Events
+        $("#calcFees").click(TotalLateItems)
+        $("#getCoworkerCount").click(TotalCoworkers)
+        $("#calcPizzaCost").click(PizzaMath)
+        //Functions
+
+
+        function TotalLateItems(){
+            var totalDue = 0
+            var lateBooks = parseInt($("#lateBooks").val())
+            totalDue+=lateBooks * 0.25
+            var lateDVDs = parseInt($("#lateDVDs").val())
+            totalDue+=lateDVDs *0.50
+            $(".totalFees").text(totalDue.toFixed(2))
+            $(".hideFees").show();
+        }
+
+
+//Pizza Stuff
+        var cheesePricePerCoworker = 15;
+        var coworkers = 0;
+        function TotalCoworkers() {
+            var totalCoworkers = 0
+            totalCoworkers = parseInt($("#pizzaEaters").val())
+            coworkers = totalCoworkers;
+            cheesePricePerCoworker = 15 / totalCoworkers
+            var coworkerNumber = 1
+            $(".coWorkerNumber").text(coworkerNumber)
+            while (totalCoworkers > 0) {
+                switch (coworkerNumber) {
+                    case 1:
+                        $(".coworkerOne").show();
+                        break;
+                    case 2:
+                        $(".coworkerTwo").show();
+                        break;
+                    case 3:
+                        $(".coworkerThree").show();
+                        break;
+                    case 4:
+                        $(".coworkerFour").show();
+                        break;
+                    case 5:
+                        $(".coworkerFive").show();
+                        break;
+                    case 6:
+                        $(".coworkerSix").show();
+                        break;
+                    case 7:
+                        $(".coworkerSeven").show();
+                        break;
+                    case 8:
+                        $(".coworkerEight").show();
+                        break;
+                    default:
+                        break;
+                }
+                coworkerNumber++;
+                totalCoworkers--;
+            }
+        }
+            function PizzaMath(){
+                var coworkerOneDues = cheesePricePerCoworker + (parseInt($("#coworkerOneInput").val()) * 1.25)
+                var coworkerTwoDues = cheesePricePerCoworker + (parseInt($("#coworkerTwoInput").val()) * 1.25)
+                var coworkerThreeDues = cheesePricePerCoworker + (parseInt($("#coworkerThreeInput").val()) * 1.25)
+                var coworkerFourDues = cheesePricePerCoworker + (parseInt($("#coworkerFourInput").val()) * 1.25)
+                var coworkerFiveDues = cheesePricePerCoworker + (parseInt($("#coworkerFiveInput").val()) * 1.25)
+                var coworkerSixDues = cheesePricePerCoworker + (parseInt($("#coworkerSixInput").val()) * 1.25)
+                var coworkerSevenDues = cheesePricePerCoworker + (parseInt($("#coworkerSevenInput").val()) * 1.25)
+                var coworkerEightDues = cheesePricePerCoworker + (parseInt($("#coworkerEightInput").val()) * 1.25)
+                var coworkerNumber = 1
+                while(coworkers > 0){
+                    switch(coworkerNumber){
+                        case 1:
+                            $(".finalOne").show();
+                            $(".coworkerOneOutput").text(coworkerOneDues.toFixed(2 ))
+                            break;
+                        case 2:
+                            $(".finalTwo").show();
+                            $(".coworkerTwoOutput").text(coworkerTwoDues.toFixed(2 ))
+                            break;
+                        case 3:
+                            $(".finalThree").show();
+                            $(".coworkerThreeOutput").text(coworkerThreeDues.toFixed(2 ))
+                            break;
+                        case 4:
+                            $(".finalFour").show();
+                            $(".coworkerFourOutput").text(coworkerFourDues.toFixed(2 ))
+                            break;
+                        case 5:
+                            $(".finalFive").show();
+                            $(".coworkerFiveOutput").text(coworkerFiveDues.toFixed(2 ))
+                            break;
+                        case 6:
+                            $(".finalSix").show();
+                            $(".coworkerSixOutput").text(coworkerSixDues.toFixed(2 ))
+                            break;
+                        case 7:
+                            $(".finalSeven").show();
+                            $(".coworkerSevenOutput").text(coworkerSevenDues.toFixed(2 ))
+                            break;
+                        case 8:
+                            $(".finalEight").show();
+                            $(".coworkerEightOutput").text(coworkerEightDues.toFixed(2 ))
+                            break;
+                        default: break;
+                    }
+                    coworkerNumber++
+                    coworkers--
+                }
+            }
+        }
+
+)
